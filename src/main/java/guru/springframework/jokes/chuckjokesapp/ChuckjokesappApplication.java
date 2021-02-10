4b9384c495760bf0988f324e53fc0fe5ea72e91d
mk <kitamarekweb@gmail.com>
2021-02-10 20:59:56 +0100

    first commit

---
+++ src/main/java/guru/springframework/jokes/chuckjokesapp/ChuckjokesappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokes.chuckjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckjokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckjokesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/jokes/chuckjokesapp/ChuckjokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokes.chuckjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckjokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
